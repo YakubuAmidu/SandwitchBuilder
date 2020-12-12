4931447d1c23969a38ef5faa263d8b5d03c48e11
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-12 01:13:40 -0800

    Commiting changes

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 import './BuildControls';
-import BuildControls from "./BuildControl/BuildControl";
+import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
   { label: 'Salad', type: 'salad' },
   { label: 'Bacon', type: 'bacon' },
   { label: 'Cheese', type: 'cheese' },
-  { label: 'Meat', type: 'meat' }
+  { label: 'Meat', type: 'meat' },
 ];
 
 const BuildControls = (props) => {
   return(
     <div className="BuildControls">
       {controls.map(ctrl => {
+        return (
         <BuildControl key={ctrl.label} label={ctrl.label} />
+      )
       })}
     </div>
   )
