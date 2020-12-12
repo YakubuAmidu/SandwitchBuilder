8efe4e315830ffc04aadd2af6e0ed2f1dbed4686
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-12 05:09:44 -0800

    Initial commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import Aux from "../../hoc/Aux";
+import Aux from "../../../hoc/Aux";
 
 const OrderSummary = (props) => {
- const ingredientSummary = Object.keys(props.ingredients).
- map((igkey) => {
+ const ingredientSummary = Object.keys(props.ingredients)
+ .map((igkey) => {
    return (
      <li>
-       <span>{igkey}</span>: {props.ingredients[igkey]}
+       <span
+       style={{ textTransform: "capitalize" }}>
+       {igkey}</span>: {props.ingredients[igkey]}
      </li>
    );
  });
@@ -16,7 +18,10 @@ const OrderSummary = (props) => {
     <Aux>
        <h3>Your order</h3>
        <p>A delicious burger with the following ingredients</p>
-
+      <ul>
+        {ingredientSummary}
+        <p>Continue to checkout</p>
+      </ul>
     </Aux>
   );
 };
