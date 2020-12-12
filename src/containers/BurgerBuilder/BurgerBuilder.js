8e1c060b0aefc3907c4d1f0e74a7a9f34e1881e1
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-12 01:35:08 -0800

    Intial commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
    state = {
      ingredients: {
@@ -11,7 +18,9 @@ class BurgerBuilder extends Component {
        bacon: 0,
        cheese: 0,
        meat: 0
-     }
+     },
+
+     totalPrice: 4
    }
 
    addIngredientHandler = (type) => {
