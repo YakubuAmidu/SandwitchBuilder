b2d35c262c473b45436e70e5c74b5674e918bf23
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-13 00:38:22 -0800

    Commiting changes

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import burgerLogo from "../../assets/images/burger-logo.png";
+
+const Logo = (props) => {
+  return(
+    <div>
+    <img src={burgerLogo} alt="burder-img" />
+  )
+}
+
+export default Logo;
