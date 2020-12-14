096a964522a21f5a35f963ae3fa4d623436b2c57
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-13 23:41:38 -0800

    Initial commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import './Layout.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className='Content'>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+   state = {
+     showSideDrawer: true
+   }
+
+   SideDrawerClosedHandler = () => {
+     this.setState({ showSideDrawer: false })
+   }
+
+  render(){
+    return(
+        <Aux>
+          <Toolbar />
+          <SideDrawer />
+          <main className='Content'>{this.props.children}</main>
+        </Aux>
+      );
+  }
+}
 
 export default layout;
