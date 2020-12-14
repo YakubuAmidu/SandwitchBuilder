0b209485719955e99148d9a105b4dc1df50dc504
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-14 01:36:57 -0800

    Initial commit

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+import './DrawerToggle.css';
+
 const DrawerToggle = (props) => {
   return(
-    <div onClick={props.clicked}>MENU</div>
+    <div className="DrawerToggle"
+    onClick={props.clicked}>
+       <div></div>
+       <div></div>
+       <div></div>
+    </div>
   )
 }
 
